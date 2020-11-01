d7b9c1e6216cbc5fa6d7e311b5f50baf924c1bde
Miclea <georgemiclea2@gmail.com>
2020-11-01 03:07:41 +0200

    added doctor & rabdare

--- src/app/domain/user.model.ts
+++ src/app/domain/user.model.ts
@@ -7,3 +7,23 @@ export class User {
   isDoctor: boolean;
   token?: string;
 }
+
+interface PatientDetails {
+  healthIsuranceId: string;
+  doctorId: number;
+}
+
+export type Patient = User & PatientDetails;
+
+enum DoctorStatus {
+  REGISTERED,
+  CONFIRMED,
+  DENIED
+}
+
+interface DoctorDetails {
+  pic: string;
+  doctorStatus: DoctorStatus;
+}
+
+export type Doctor = User & DoctorDetails;
